6d4897061b154ad02e3958499bbf818da38c48a8
thumbthing <ilscc0902@gmail.com>
2023-04-07 13:31:06 +0900

    fix: App.tsx (Router 추가)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MainPage from './pages/Main';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={MainPage} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Signup.tsx
@@ -0,0 +1,16 @@
+
+export default function Signup() {
+
+  return (
+    <div>
+      <h2> 아이디 </h2>
+      <input data-testid="email-input" />
+      <h2> 비밀번호 </h2>
+      <input data-testid="password-input" />
+      <div>
+        <button data-testid="signin-button" placeholder="로그인" onClick={() => alert("로그인")} />
+        <button data-testid="signup-button" placeholder="회원가입" onClick={() => alert("회원 가입")} />
+      </div>
+    </div>
+  )
+}
