91edacb317b9e7d0ba77b6b6c1a6bcf85235b69a
thumbthing <ilscc0902@gmail.com>
2023-04-08 01:33:45 +0900

    feat: Login.tsx (alert 로 유효성 검사)

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,46 @@
+import React, { useState } from "react";
+
+interface FormValue {
+  id: string;
+  password: string;
+}
 
 export default function Login() {
+  const [ id, setId ] = useState('');
+  const [ password, setPassword ] = useState('');
 
+  const CheckValidEmail = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+    setId(e.target.value);
+  }
+  
+  const CheckValidPassword = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+    setPassword(e.target.value);
+  }
+  
+  const SetValidUserInformation = () => {
+    alert(id + password);
+    ( !id.includes("@") ? alert("유효하지 않은 아이디입니다.") : alert("Id is valid") );
+    ( password.length < 8 ? alert("비밀번호가 너무 짧습니다.") : alert("Password is valid") );
+  }
   return (
-    <div>
-      <h2> 아이디 </h2>
-      <input data-testid="email-input" />
-      <h2> 비밀번호 </h2>
-      <input data-testid="password-input" />
-      <div>
-        <button data-testid="signin-button" placeholder="로그인" onClick={() => alert("로그인")} />
-        <button data-testid="signup-button" placeholder="회원가입" onClick={() => alert("회원 가입")} />
-      </div>
-    </div>
+    <>
+      <input 
+        data-testid = "email-input"
+        value = { id }
+        onChange={ CheckValidEmail }
+      />
+      <input
+        data-testid = "password-input"
+        value = { password }
+        onChange = { CheckValidPassword }
+      />
+      <button 
+        data-testid = "signup-button"
+        onClick={ SetValidUserInformation }
+      >
+      회원 가입  
+      </button>
+  
+    </>
   )
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -12,3 +12,8 @@ export default function MainPage() {
     </PageLayout>
   )
 }
+
+const StyledPageLayout = styled.div`
+  display: flex;
+`
+
