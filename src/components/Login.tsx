5695a428d34a916dbbcbefc769c5aa2eb4f89568
thumbthing <ilscc0902@gmail.com>
2023-04-07 13:06:15 +0900

    fix: Login.tsx (data-testid 수정)

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,9 +6,11 @@ export default function Login() {
       <h2> 아이디 </h2>
       <input data-testid="email-input" />
       <h2> 비밀번호 </h2>
-      <input data-testid="email-input" />
-      <button placeholder="로그인" onClick={() => alert("로그인")} />
-      <button placeholder="회원가입" onClick={() => alert("회원 가입")} />
+      <input data-testid="password-input" />
+      <div>
+        <button data-testid="signin-button" placeholder="로그인" onClick={() => alert("로그인")} />
+        <button data-testid="signup-button" placeholder="회원가입" onClick={() => alert("회원 가입")} />
+      </div>
     </div>
   )
 }
