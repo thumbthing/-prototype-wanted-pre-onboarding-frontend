65d4aef2212f0da6acf158a59e2db96d60a73043
thumbthing <ilscc0902@gmail.com>
2023-04-08 03:13:55 +0900

    feat: TodoPage.tsx (Route 용 페이지 생성)

---
+++ src/pages/TodoPage.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import PageLayout from "../components/PageLayout"
+
+
+export default function TodoPage() {
+  return (
+    <PageLayout>
+      <Header />
+      <div>
+      this is todo Page
+      </div>
+    </PageLayout>
+  )
+}
